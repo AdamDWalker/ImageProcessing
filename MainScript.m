3bb5f6c53ccc7d545bc99a72ac203cc7024ee86c
AdamDWalker <adamwalker03@googlemail.com>
2016-11-22 14:54:12 +0000

    Reset sharpening filter code + general refactoring
    - Sharpening code was broken, has been reset to empty loop
    - Changed from multiple figure windows to one subplotted collection of output images
    - Output is easier to follow now

--- MainScript.m
+++ MainScript.m
@@ -2,62 +2,40 @@
 close all;
 
 % Step-1: Load input image
-I = imread('AssignmentInput.jpg');
-figure;
-imshow(I);
-title('Step-1: Load input image');
+% I = imread('AssignmentInput.jpg');
+% figure;
+% imshow(I);
+% title('Step-1: Load input image');
 
 % Step-2: Conversion of input image to greyscale
-figure;
+subplot(2, 2, 1);
 grayImage = rgb2gray(I);
 imshow(grayImage);
-title('Step-2: Conversion of input image to greyscale');
-figure;
+title('Grayscale Input');
+subplot(2, 2, 2);
 imhist(grayImage);
+title('Grayscale hist');
 % Step-3: Noise removal
 
 
 % Step-4: Image Sharpening
-
-highest = 0;
-lowest = 0;
-mean = 0;
-threshold = 5;
 for loopcount = 0 : 1
     for row = 2 : size(grayImage, 1) - 1
         for col = 2 : size(grayImage, 2) - 1      
             for a = row - 1 : row + 1
                 for b = col - 1 : col + 1
-
-                  if grayImage(a, b) > highest
-                      highest = grayImage(a, b);
-                  end
-                  
-                  if grayImage(a, b) < lowest
-                      lowest = grayImage(a, b);
-                  end
                 end
             end
-            mean = (highest + lowest) / 2;
-            
-            if (grayImage(row, col) >= threshold)
-                grayImage(row, col) = grayImage(row, col) + mean;
-            else
-                grayImage(row, col) = grayImage(row, col) - mean;
-            end      
-            
-            highest = 0;
-            lowest = 0;
-            mean = 0;
         end
     end
 end
 
-figure;
-imshow(image);
-title('Hopefully a sharpened image');
-figure 
-imhist(image);
+subplot(2, 2, 3);
+imshow(grayImage);
+title('Sharpened image');
+subplot(2, 2, 4);
+imhist(grayImage);
+title('Sharpened hist');
 
 % Step-whatever: Image smoothing
 % image = im2double(grayImage);

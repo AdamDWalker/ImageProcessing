8080a393229e4a611b240b130c9c7cc42f7e0dd1
AdamDWalker <adamwalker03@googlemail.com>
2016-11-22 14:39:10 +0000

    Sharpening filter not working - Not sure why

--- MainScript.m
+++ MainScript.m
@@ -12,44 +12,67 @@ figure;
 grayImage = rgb2gray(I);
 imshow(grayImage);
 title('Step-2: Conversion of input image to greyscale');
-
+figure;
+imhist(grayImage);
 % Step-3: Noise removal
 
 
 % Step-4: Image Sharpening
-image = im2double(grayImage);
-outputImage = zeros(size(image));
 
+highest = 0;
+lowest = 0;
+mean = 0;
+threshold = 5;
 for loopcount = 0 : 1
-    for row = 2 : size(image, 1) - 1
-        for col = 2 : size(image, 2) - 1      
+    for row = 2 : size(grayImage, 1) - 1
+        for col = 2 : size(grayImage, 2) - 1      
             for a = row - 1 : row + 1
                 for b = col - 1 : col + 1
 
-                    
-
+                  if grayImage(a, b) > highest
+                      highest = grayImage(a, b);
+                  end
+                  
+                  if grayImage(a, b) < lowest
+                      lowest = grayImage(a, b);
+                  end
                 end
             end
-            image(row, col) = sum;
+            mean = (highest + lowest) / 2;
             
+            if (grayImage(row, col) >= threshold)
+                grayImage(row, col) = grayImage(row, col) + mean;
+            else
+                grayImage(row, col) = grayImage(row, col) - mean;
+            end      
+            
+            highest = 0;
+            lowest = 0;
+            mean = 0;
         end
     end
 end
 
+figure;
+imshow(image);
+title('Hopefully a sharpened image');
+figure 
+imhist(image);
+
 % Step-whatever: Image smoothing
 % image = im2double(grayImage);
 % outputImage = zeros(size(image));
-
+% 
 % sum = 0;
-
+% 
 % for loopcount = 0 : 1
 %    for row = 2 : size(image, 1) - 1
 %       for col = 2 : size(image, 2) - 1      
 %           for a = row - 1 : row + 1
 %               for b = col - 1 : col + 1
-
+% 
 %                   sum = sum + image(a, b);
-
+% 
 %               end
 %           end
 %           sum = sum / 9;
@@ -58,7 +81,7 @@ end
 %        end
 %    end
 % end
-
+% 
 % figure;
 % imshow(image);
 % title('Hopefully a smoothed image');

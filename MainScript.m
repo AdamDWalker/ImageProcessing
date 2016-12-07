741990cc82ac6224b9d23f59adf64112869c978c
AdamDWalker <adamwalker03@googlemail.com>
2016-12-07 17:42:57 +0000

    Step-7: Recognise Starfish obects working
    - Using matlab code to identify object by roundness
    - Found min and max values for starfish roundness
    - Mark all objects in the image outside that range
    - Need to find a way to remove those objects then it's complete

--- MainScript.m
+++ MainScript.m
@@ -131,7 +131,7 @@ for row = 1 : size(sharp, 1)
 end
 
 % Invert the image such that black is the background
-BW = ~BW;
+BW = ~BW ;
 
 figure;
 imshow(BW);
@@ -148,3 +148,71 @@ IM7 = bwareaopen(IM6, 200);
 figure;
 imshow(IM7);
 title('Dilated');
+
+% Step-7: Remove non-starfish objects
+
+[B,L] = bwboundaries(IM7, 'noholes');
+hold on
+for k = 1:length(B)
+  boundary = B{k};
+  plot(boundary(:,2), boundary(:,1), 'w', 'LineWidth', 2)
+end
+
+stats = regionprops(L,'Area','Centroid', 'Perimeter');
+
+threshold = 0.94;
+minThresh = 0.18;
+maxThresh = 0.24;
+
+% loop over the boundaries
+for k = 1:length(B)
+
+  % obtain (X,Y) boundary coordinates corresponding to label 'k'
+  boundary = B{k};
+
+  % compute a simple estimate of the object's perimeter
+  delta_sq = diff(boundary).^2;
+  perimeter = stats(k).Perimeter;
+
+  % obtain the area calculation corresponding to label 'k'
+  area = stats(k).Area;
+
+  % compute the roundness metric
+  metric = 4*pi*area/perimeter^2;
+
+  % display the results
+  metric_string = sprintf('%2.2f',metric);
+
+  % mark objects outside the threshold range with a black circle
+  if metric < minThresh || metric > maxThresh
+    centroid = stats(k).Centroid;
+    plot(centroid(1),centroid(2),'ko');     
+  end
+
+  text(boundary(1,2)-35,boundary(1,1)+13,metric_string,'Color','y',...
+       'FontSize',14,'FontWeight','bold');
+
+end
+
+title(['Metrics closer to 1 indicate that ',...
+       'the object is approximately round']);
+
+% label = bwlabel(IM7);
+% props = regionprops(label, 'Area', 'Perimeter');
+% 
+% area = [props.Area];
+% perimeter = [props.Perimeter];
+% roundness = 4*pi*area/perimeter.^2;
+% 
+% display(area);
+% display(perimeter);
+% display(roundness);
+% 
+% target = find(((900 <= area) & (area <= 1200)) & ((0.05 <= metric) & (metric <= 0.15)));
+%  
+% IM8 = ismember(label, target);
+figure;
+imshow(IM8);
+title('Step-7: Automatic Recognition');
+
+

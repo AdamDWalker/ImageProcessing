37f6565db345587c5eebb774d83cdc303ccd53f0
AdamDWalker <adamwalker03@googlemail.com>
2016-12-08 00:11:49 +0000

    Step 7 Starfish recognition completed
    - Starfish objects detected & other objects removed
    - Using regionprops to detect each object
    - Create array of roundness metrics
    - Use ismember function to retain all objects in correct roundness range

--- MainScript.m
+++ MainScript.m
@@ -147,72 +147,23 @@ IM7 = bwareaopen(IM6, 200);
 
 figure;
 imshow(IM7);
-title('Dilated');
+title('Step-6: Morphological Processing');
 
-% Step-7: Remove non-starfish objects
+% Step-7: Starfish recognition
 
-[B,L] = bwboundaries(IM7, 'noholes');
-hold on
-for k = 1:length(B)
-  boundary = B{k};
-  plot(boundary(:,2), boundary(:,1), 'w', 'LineWidth', 2)
+IM8 = IM7;
+L = bwlabel(IM8);
+S = regionprops(L, 'Area', 'Perimeter');
+metrics = zeros(1,25);
+
+area = [S.Area];
+perimeter = [S.Perimeter];
+for i = 1 : length(metrics)
+    metrics(i) = 4*pi*area(i)/perimeter(i).^2;
 end
+idx = find(( metrics > 0.18)  & (metrics < 0.24));
+IM9 = ismember(L, idx);
 
-stats = regionprops(L,'Area','Centroid', 'Perimeter');
-
-threshold = 0.94;
-minThresh = 0.18;
-maxThresh = 0.24;
-
-% loop over the boundaries
-for k = 1:length(B)
-
-  % obtain (X,Y) boundary coordinates corresponding to label 'k'
-  boundary = B{k};
-
-  % compute a simple estimate of the object's perimeter
-  delta_sq = diff(boundary).^2;
-  perimeter = stats(k).Perimeter;
-
-  % obtain the area calculation corresponding to label 'k'
-  area = stats(k).Area;
-
-  % compute the roundness metric
-  metric = 4*pi*area/perimeter^2;
-
-  % display the results
-  metric_string = sprintf('%2.2f',metric);
-
-  % mark objects outside the threshold range with a black circle
-  if metric < minThresh || metric > maxThresh
-    centroid = stats(k).Centroid;
-    plot(centroid(1),centroid(2),'ko');     
-  end
-
-  text(boundary(1,2)-35,boundary(1,1)+13,metric_string,'Color','y',...
-       'FontSize',14,'FontWeight','bold');
-
-end
-
-title(['Metrics closer to 1 indicate that ',...
-       'the object is approximately round']);
-
-% label = bwlabel(IM7);
-% props = regionprops(label, 'Area', 'Perimeter');
-% 
-% area = [props.Area];
-% perimeter = [props.Perimeter];
-% roundness = 4*pi*area/perimeter.^2;
-% 
-% display(area);
-% display(perimeter);
-% display(roundness);
-% 
-% target = find(((900 <= area) & (area <= 1200)) & ((0.05 <= metric) & (metric <= 0.15)));
-%  
-% IM8 = ismember(label, target);
 figure;
-imshow(IM8);
-title('Step-7: Automatic Recognition');
-
-
+imshow(IM9);
+title('Step-7 Automatic recognition');
